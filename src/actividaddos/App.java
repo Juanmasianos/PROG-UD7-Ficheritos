d2927713d779e68368ac3cd228c0c1ae008fd95d
Juan Manuel <usr3507@salesianos-lacuesta.net>
2025-06-10 11:00:07 +0100

    feat: second activity done

--- src/actividaddos/App.java
+++ src/actividaddos/App.java
@@ -1,5 +1,47 @@
 package actividaddos;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class App {
-    
+    public static void main(String[] args) {
+        
+        File fileTwo = new File("./src/actividaddos/file/ficherodos.txt");
+
+        Scanner scanner = new Scanner(System.in);
+
+        try (FileInputStream fInStream = new FileInputStream(fileTwo)) {
+
+            for (int i = 0; i < fileTwo.length(); i++) {
+
+                char currentChar = (char) fInStream.read();
+
+                if (currentChar != ' ') {
+
+                    System.out.print(currentChar + "_" + (int) currentChar + ", ");
+                    
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("No se ha encontrado el fichero");
+
+        } catch (IOException e) {
+
+            System.out.println("Ha ocurrido un error al escribir el fichero\n" + e);
+
+        }
+
+        scanner.close();
+
+    }
 }
